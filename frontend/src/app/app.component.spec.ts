43b64eef7d5007c7612730d8e4a741078ec881c9
0belissk <137469655+0belissk@users.noreply.github.com>
2025-10-02 15:23:46 -0500

    test(frontend): stub auth service in app component spec

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -3,12 +3,20 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar.component';
+import { AuthService } from './auth/auth.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    const authServiceStub: Partial<AuthService> = {
+      isLoggedIn: () => false,
+      login: () => void 0,
+      logout: () => void 0,
+    };
+
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, NavbarComponent],
       declarations: [AppComponent],
+      providers: [{ provide: AuthService, useValue: authServiceStub }],
     }).compileComponents();
   });
 
