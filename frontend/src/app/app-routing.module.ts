8405127cedbe9ef7744e41d7012355f9277f0298
0belissk <137469655+0belissk@users.noreply.github.com>
2025-10-02 19:03:21 -0500

    wire: add AuthGuard, register JwtInterceptor, initialize AuthService

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home.component').then(m => m.HomeComponent) },
-  { path: 'send', loadComponent: () => import('./pages/send-stats.component').then(m => m.SendStatsComponent) },
+  { path: 'send', loadComponent: () => import('./pages/send-stats.component').then(m => m.SendStatsComponent), canActivate: [AuthGuard] },
   { path: 'auth/callback', loadComponent: () => import('./pages/auth-callback.component').then(m => m.AuthCallbackComponent) },
   { path: '**', redirectTo: '' }
 ];
