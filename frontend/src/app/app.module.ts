10d59fedc5d9456a7e199aa82dbedb41934aa40e
0belissk <137469655+0belissk@users.noreply.github.com>
2025-10-02 19:05:30 -0500

    wire: add AuthGuard, register JwtInterceptor, initialize AuthService

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { JwtInterceptor } from './auth/jwt.interceptor';
+import { AuthService } from './auth/auth.service';
+
+export function initAuth(auth: AuthService) {
+  return () => auth.init();
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: APP_INITIALIZER, useFactory: initAuth, deps: [AuthService], multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
