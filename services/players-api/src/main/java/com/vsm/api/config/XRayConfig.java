2211ef936c9b66694317b4fa04b23d07526f356d
Paul Quinto <pq42@Pauls-MacBook-Pro.local>
2025-10-20 22:33:53 -0500

    fix: align xray filter configuration with servlet api

--- services/players-api/src/main/java/com/vsm/api/config/XRayConfig.java
+++ services/players-api/src/main/java/com/vsm/api/config/XRayConfig.java
@@ -4,6 +4,9 @@ import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorder;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.strategy.FixedSegmentNamingStrategy;
+import com.amazonaws.xray.strategy.SegmentNamingStrategy;
+import javax.servlet.Filter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,15 +17,15 @@ import org.springframework.core.Ordered;
 public class XRayConfig {
 
   private final String daemonAddress;
-  private final String serviceName;
+  private final SegmentNamingStrategy segmentNamingStrategy;
   private final String samplingStrategyLocation;
 
   public XRayConfig(
       @Value("${AWS_XRAY_DAEMON_ADDRESS:127.0.0.1:2000}") String daemonAddress,
-      @Value("${XRAY_SERVICE_NAME:players-api}") String serviceName,
+      @Value("${XRAY_SERVICE_NAME:players-api}") String segmentName,
       @Value("${XRAY_SAMPLING_STRATEGY:default}") String samplingStrategyLocation) {
     this.daemonAddress = daemonAddress;
-    this.serviceName = serviceName;
+    this.segmentNamingStrategy = new FixedSegmentNamingStrategy(segmentName);
     this.samplingStrategyLocation = samplingStrategyLocation;
   }
 
@@ -39,10 +42,10 @@ public class XRayConfig {
   }
 
   @Bean
-  public FilterRegistrationBean<AWSXRayServletFilter> awsXRayFilter(AWSXRayRecorder recorder) {
-    AWSXRayServletFilter filter = new AWSXRayServletFilter(serviceName, recorder);
-    FilterRegistrationBean<AWSXRayServletFilter> registration =
-        new FilterRegistrationBean<>(filter);
+  public FilterRegistrationBean<Filter> awsXRayFilter(AWSXRayRecorder recorder) {
+    AWSXRayServletFilter filter = new AWSXRayServletFilter(segmentNamingStrategy, recorder);
+    FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+    registration.setFilter(filter);
     registration.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
     return registration;
   }
